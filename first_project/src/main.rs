3b76fe63d506adbf5d436dab940d51e077eadbe2
Rushhaabhhh <rushabhmistry16@gmail.com>
2024-12-03 02:30:37 +0530

    Jargons

--- first_project/src/main.rs
+++ first_project/src/main.rs
@@ -29,7 +29,7 @@ fn main() {
 
     // Tuples
     let tup: (i32, f64, u8) = (500, 6.4, 1);
-    let (x, y, z) = tup;
+    let (_x, y, _z) = tup;
     println!("The value of y is: {}", y);
 
     // Conditionals
@@ -46,4 +46,28 @@ fn main() {
         counter += 1;
     println!("Counter is {}", counter);
     }
+
+    // Functions
+    fn add(x: i32, y: i32) -> i32 {
+        x + y
+    }
+    println!("The sum of 5 and 6 is {}", add(5, 6));
+
+    // Memory Management
+
+        // In java, javascript, memory is managed by garbage collector where data is allocated and deallocated automatically. It is safe but slow.
+        // In C, C++, memory is managed by programmer where data is allocated and deallocated manually. It is prone to memory leaks and dangling pointers
+        // In rust, memory is managed by compiler where data is allocated and deallocated automatically. It is safe and fast.
+        // Rust achieves this by using Mutability, Ownership, Borrowing, Lifetimes, and Heap.
+
+    // Stack and Heap
+
+        // Rust stores data in stack and heap. In stack non-fixed size data is stored and in heap fixed size data is stored. Stack is faster than heap. 
+        // Stack is managed by compiler and heap is managed by OS.
+        // Stack : Variables, Functions, Pointers, etc.
+        // Heap : Strings, Vectors, etc.
+
+    // Ownership
+
+        // Each peice of data in Rust has a single owner. There can only be one owner at a time. When the owner goes out of scope, Rust automatically deallocates the data, preventing memory leaks.
 }
